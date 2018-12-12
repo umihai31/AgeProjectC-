513bf401df86324469e91133d48cc92b8d8caa72
Ureche Mihai <mihai.ovidiu@hotmail.com>
2018-12-12 19:13:20 +0200

    Base project

--- AgeProject/Program.cs
+++ AgeProject/Program.cs
@@ -8,8 +8,72 @@ namespace AgeProject
 {
     class Program
     {
+        public enum Gender {Female, Male}
+
         static void Main(string[] args)
         {
+            string BirthYearString, BirthMonthString, BirthDayString; //Variables for reading from keyboard the year,month and day
+            string FirstName, LastName; // Variable used for First Name and Last Name
+            bool ParseTry1, ParseTry2, ParseTry3; // For trying to parse
+            DateTime BornDate;   
+
+            Console.WriteLine("Hello ,please give us your name");
+
+            Console.WriteLine("First Name:");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("Last Name : ");
+            LastName = Console.ReadLine();
+
+            Console.WriteLine("Good job {0} !! \nNow we need your Age ",FirstName);
+
+            //Read the year from keyboard and try to convert this string to int 
+            Console.WriteLine("Give us your birthday year :");
+            BirthYearString = Console.ReadLine();
+            ParseTry1 = Int32.TryParse(BirthYearString,out int BirthYear);
+            
+            while (!ParseTry1)
+            {
+                Console.WriteLine("This is not a valid year , give us another one :");
+                BirthYearString = Console.ReadLine();
+                ParseTry1 = Int32.TryParse(BirthYearString, out BirthYear);
+            }
+
+            //Read the month from keyboard and try to convert this string to int
+            Console.WriteLine("Now your birth month !");
+            BirthMonthString = Console.ReadLine();
+            ParseTry2 = Int32.TryParse(BirthMonthString, out int BirthMonth);
+
+            while (!ParseTry2)
+            {
+                Console.WriteLine("This is not a valid month , give us another one :");
+                BirthYearString = Console.ReadLine();
+                ParseTry2 = Int32.TryParse(BirthYearString, out BirthYear);
+            }
+
+            //Read the day from keyboard and try to convert this string to int 
+            Console.WriteLine("Now your birth day !");
+            BirthDayString = Console.ReadLine();
+            ParseTry3 = Int32.TryParse(BirthDayString, out int BirthDay);
+
+            while (!ParseTry3)
+            {
+                Console.WriteLine("This is not a valid day , give us another one :");
+                BirthYearString = Console.ReadLine();
+                ParseTry2 = Int32.TryParse(BirthYearString, out BirthYear);
+            }
+
+            BornDate = new DateTime(BirthYear,BirthMonth,BirthDay);
+            Console.WriteLine("Thank you !!\nYou were born in {0}", BornDate);
+
+
+
+
+
+
+
+
+            Console.ReadLine(); //For keeping the the window opened till you press enter 
         }
     }
 }
